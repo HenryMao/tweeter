dc1d8783e876a73be476ae45e5c96659637024da
HenryMao <henry60308@live.cn>
2020-07-14 23:41:12 +0000

    shadow done

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(){
+    $("output").val(140-$(this).val().length);
+    if(140-$(this).val().length < 0){
+      $("output").css("color", "red");
+    }else{
+      $("output").css("color", "black");
+    }
+  });
+});
