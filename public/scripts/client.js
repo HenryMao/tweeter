c9b38df5d45a6cba7eab8bc7009f3a88f0d382ef
HenryMao <henry60308@live.cn>
2020-07-15 23:37:12 +0000

    dynamically working with get and post ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,7 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function(){
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+
   
 
   const createTweetElement = function(tweet){
@@ -59,11 +36,23 @@ $(document).ready(function(){
 const renderTweet = function(tweets){
   for(let tweet of tweets){
     let $result = createTweetElement(tweet);
-    $('.tweets').append($result); 
+    $('.tweets').prepend($result); 
   }
 }
-renderTweet(tweetData);
 
+
+const loadTweets = function(){
+  $.ajax({
+    url:'/tweets/',
+    method:'GET'
+  })
+  .then(function(tweetData){
+    $(".tweets").empty();
+    renderTweet(tweetData);
+  })
+}
+
+loadTweets();
 
 $(".new-tweet-form").submit(function(event){
   event.preventDefault();
@@ -73,13 +62,11 @@ $(".new-tweet-form").submit(function(event){
     method: 'POST',
     data: $(this).serialize()})
   .then(function(response) {
-    console.log(response);
-    
+    console.log("pls");
+    loadTweets();
 })
 
 })
-const loadTweets = function(){
 
-}
 
 });
