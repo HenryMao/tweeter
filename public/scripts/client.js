6902b4d1397930e9c4d0ddc7399e15dd8027018f
HenryMao <henry60308@live.cn>
2020-07-16 22:14:01 +0000

    responsivee

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,12 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function(){
 
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
   
 
   const createTweetElement = function(tweet){
@@ -14,14 +20,14 @@ $(document).ready(function(){
     let handle = tweet.user.handle;
     let content = tweet.content.text;
     let timestamp = tweet.created_at;
-  
+    let contentFiltered = `${escape(content)}`;
     let $new_tweet = $(`<article class = "tweet">
       <header class="tweets-header">
       <h6 class = "creator-handle"><img class="tweets-pic" src="${image}"> ${name}</h6>
       <h6 class = "handle">${handle}</h6>
       </header>
       <p>
-      ${content}
+      ${contentFiltered}
       </p>
       <footer class="tweets-footer">
       <p class="footer-element">${timestamp}</p>
@@ -58,12 +64,19 @@ loadTweets();
 $(".new-tweet-form").submit(function(event){
   event.preventDefault();
   if($("#tweet-text").val().length > 140){
-    alert("Too long!");
+    let $errorMessage = $(`<div id="errorM">Too long...</div>`);
+    $(".errorMessage").prepend($errorMessage);
+    $("#errorM").show().delay(3000).fadeOut();
     
   } else if($("#tweet-text").val().length  <= 0){
-    alert("Too short!");
+    let $errorMessage = $(`<div id="errorM">Please enter something to tweet...</div>`);
+    $(".errorMessage").prepend($errorMessage);
+    $("#errorM").show().delay(3000).fadeOut();
+
   }
   else {
+    $(".errorMessage").empty();
+    console.log($(this).serialize());
     $.ajax({
       url: '/tweets/',
       method: 'POST',
